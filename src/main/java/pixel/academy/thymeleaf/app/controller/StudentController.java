73eb631d8071332102c9d8cfe7cd77f7aa0f011e
VictorDemerji <demerji.v1@gmail.com>
2025-12-27 13:31:18 +0200

    chore: Load the input data

--- src/main/java/pixel/academy/thymeleaf/app/controller/StudentController.java
+++ src/main/java/pixel/academy/thymeleaf/app/controller/StudentController.java
@@ -3,6 +3,8 @@ package pixel.academy.thymeleaf.app.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import pixel.academy.thymeleaf.app.model.Student;
 
 @Controller
@@ -17,6 +19,15 @@ public class StudentController {
     theModel.addAttribute("student", theStudent);
 
     return "student-form";
+}
+
+@PostMapping("/processStudentForm")
+    public String processForm(@ModelAttribute("student") Student theStudent) {
+
+    //log the input data
+    System.out.println("theStudent: " + theStudent.getFirstName() + " " + theStudent.getLastName());
+
+    return "student-confirmation";
 }
 
 }
